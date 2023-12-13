e25cc7ea534e43833a5b0034e76b948ca8699ce8
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-12-13 20:34:34 +0100

    multiple ships

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
-#include "ship.h"
-#include "board.h"
+#include "include/ship.h"
+#include "include/board.h"
 
 
 
 int main()
 {
     int boardSize = 10;
+    int numberOfShips = 0;
+
     std::cout << "Hello world!" << std::endl;
     Board playerBoard(boardSize);
     playerBoard.draw();
+
+    while (numberOfShips <= 10)
+    {
+        playerBoard.addShip();
+        playerBoard.draw();
+        numberOfShips = playerBoard.getNumberOfShips();
+    }
 
     return 0;
 }
